723d172ef5a07b76f12105332f2c804120d8f397
AuraFly <JordanJCo@gmail.com>
2022-06-02 20:18:46 -0600

    fixed randomized images in activities route, see helpers.js for reference

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,15 +7,36 @@ module.exports = {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
-  get_strImg: () => {
+
+  get_strImg: (type) => {
     const randomNum = Math.floor(Math.random() * 11);
 
+    let image1 = "";
+    let image2 = "";
+    let image3 = "";
+    switch (type) {
+      case "Strength":
+        image1 = "http://localhost:3001/images/lifting.jpg";
+        image2 = "http://localhost:3001/images/climbing.jpg";
+        image3 = "http://localhost:3001/images/boxing.jpg";
+        break;
+      case "Aerobic":
+        image1 = "http://localhost:3001/images/lifting.jpg";
+        image2 = "http://localhost:3001/images/weights.jpg";
+        image3 = "http://localhost:3001/images/climbing.jpg";
+      case "Flexibility":
+        image1 = "http://localhost:3001/images/lifting.jpg";
+        image2 = "http://localhost:3001/images/weights.jpg";
+        image3 = "http://localhost:3001/images/climbing.jpg";
+      default:
+        break;
+    }
     if (randomNum > 8) {
-      return `<img src="http://localhost:3001/images/lifting.jpg" alt="strength"></img>`;
+      return `<img src="${image1}" alt="${type}">`;
     } else if (randomNum > 4) {
-      return `<img src="http://localhost:3001/images/weights.jpg" alt="strength"></img>`;
+      return `<img src="${image2}" alt="${type}">`;
     } else {
-      return `<img src="http://localhost:3001/images/g-strength.jpg" alt="strength"></img>`;
+      return `<img src="${image3}" alt="${type}">`;
     }
   },
 };
