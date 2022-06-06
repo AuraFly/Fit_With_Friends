8f88ac36fbc0a8f10b9c93902b8b58d30fbc397a
Cynthia Godoy <cynthia@ck-interiors.com>
2022-06-06 09:40:32 -0600

    helpers and account handlebars gtype

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,8 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
   format_amount: (amount) => {
-    // format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
 
@@ -39,40 +37,45 @@ module.exports = {
       default:
         break;
     }
-    if (randomNum > 15) {
+    if (randomNum > 8) {
       return `<img src="${image1}" alt="${type}">`;
-    } else if (randomNum > 7) {
+    } else if (randomNum > 6) {
       return `<img src="${image2}" alt="${type}">`;
-    } else {
+    } else if (randomNum > 4) {
       return `<img src="${image3}" alt="${type}">`;
+    } else if (randomNum > 2) {
+      return `<img src="${image4}" alt="${type}">`;
+    } else {
+      return `<img src="${image5}" alt="${type}">`;
     }
   },
 
-get_goalImg: (gtype) => {
-  const randomNumb = Math.floor(Math.random() * 7);
+  get_goalImg: (gtype) => {
+    const randomNumb = Math.floor(Math.random() * 11);
 
-  let imag1 = "";
-  let imag2 = "";
-  switch (gtype) {
-    case "Stamina":
-      imag1 = "http://localhost:3001/images/g-stamina.jpg";
-      imag2 = "http://localhost:3001/images/g-stamina2.jpg";
-      break;
-    case "Strength":
-      imag1 = "http://localhost:3001/images/g-strength.jpg";
-      imag2 = "http://localhost:3001/images/g-strength2.jpg";
-    case "Diet":
-      imag1 = "http://localhost:3001/images/g-diet.jpg";
-      imag2 = "http://localhost:3001/images/g-water.jpg";
-    case "Core":
-      imag1 = "http://localhost:3001/images/g-core.jpg";
-      imag2 = "http://localhost:3001/images/g-balance.jpg";
-    default:
-      break;
+    let imag1 = "";
+    let imag2 = "";
+    switch (gtype) {
+      case "Stamina":
+        imag1 = "http://localhost:3001/images/g-stamina.jpg";
+        imag2 = "http://localhost:3001/images/g-stamina2.jpg";
+        break;
+      case "Strength":
+        imag1 = "http://localhost:3001/images/g-strength.jpg";
+        imag2 = "http://localhost:3001/images/g-strength2.jpg";
+      case "Diet":
+        imag1 = "http://localhost:3001/images/g-diet.jpg";
+        imag2 = "http://localhost:3001/images/g-water.jpg";
+      case "Core":
+        imag1 = "http://localhost:3001/images/g-core.jpg";
+        imag2 = "http://localhost:3001/images/g-balance.jpg";
+      default:
+        break;
     }
     if (randomNumb > 5) {
       return `<img src="${imag1}" alt="${gtype}">`;
-    } else if (randomNumb > 0) {
+    } else {
       return `<img src="${imag2}" alt="${gtype}">`;
     }
-  }}
+  },
+}
